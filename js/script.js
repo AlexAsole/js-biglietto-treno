a0878b69b48cef37c2853836e6cebba76aae090f
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2020-12-14 15:54:35 +0100

    Con un solo DIV

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
-
 var Km = parseInt(prompt('Benvenuto nel servizio per il calcolo d\'importo del biglietto del treno più bello del Mondo! Per favore indichi la distanza che vuole percorrere!'));
 var Multiplier = 100; //per arrotondare alla seconda cifra decimale, in alcuni casi andava alla terza e non mi piaceva
 if (isNaN(Km) ){
   alert('Per favore, inserire ESCLUSIVAMENTE cifre, non testo!'); //se non è un numero si blocca
 } else {
   console.log('Kilometri: ' + Km);
-  document.getElementById('Km').innerHTML = 'Il Tuo Kilometraggio: ' + Km + ' Km';
   var Age = parseInt(prompt('Per favore, indichi la sua eta!'));
   if (isNaN(Age) ){
     alert('Per favore, inserire ESCLUSIVAMENTE cifre, non testo!');//se non è un numero si blocca
   } else {
     console.log('Età: ' + Age);
-    document.getElementById('Age').innerHTML = 'La tua Età: ' + Age;
     var Base = Km * 0.21; //calcolo del prezzo base del biglietto
     console.log('Prezzo Base: ' + Base + '€');
-    document.getElementById('Base').innerHTML = 'Il prezzo base: ' + Base + ' €';
     if (Age < 18){
       var DiscountMin = Base * 0.2;
-      Base = Math.floor((Base - DiscountMin) * Multiplier) / Multiplier; //calcolo del prezzo con lo sconto minorenni applicato
+      Discount = Math.floor((Base - DiscountMin) * Multiplier) / Multiplier; //calcolo del prezzo con lo sconto minorenni applicato
       console.log('Prezzo Scontato: ' + Base + '€');
-      document.getElementById('Discount').innerHTML = 'Sconto minorenni applicato: ' + Base + ' €';
+      document.getElementById('Price').innerHTML = 'Il Tuo Kilometraggio: ' + Km + ' Km;' + '<br>' + 'La tua Età: ' + Age + ' Anni;' + '<br>' + 'Prezzo Base: ' + Base + ' €;' + '<br>' + 'Sconto Minorenni Calcolato: ' + Discount + ' €;' + '<br>' + 'Grazie per averci scelti!' + '<br>' + 'Buonagiornata!';
     } else if (Age >=65){
       var DiscountMax = Base * 0.4;
-      Base = Math.floor((Base - DiscountMax) * Multiplier) / Multiplier; //calcolo del prezzo con lo sconto anziani applicato
+      Discount = Math.floor((Base - DiscountMax) * Multiplier) / Multiplier; //calcolo del prezzo con lo sconto anziani applicato
       console.log('Prezzo Scontato: ' + Base + '€');
-      document.getElementById('Discount').innerHTML = 'Sconto anziani (Over 65) applicato: ' + Base + ' €';
+      document.getElementById('Price').innerHTML = 'Il Tuo Kilometraggio: ' + Km + ' Km;' + '<br>' + 'La tua Età: ' + Age + ' Anni;' + '<br>' + 'Prezzo Base: ' + Base + ' €;' + '<br>' + 'Sconto Anziani (over 65) Calcolato: ' + Discount + ' €;' + '<br>' + 'Grazie per averci scelti!' + '<br>' + 'Buonagiornata!';
     } else {
       console.log('Prezzo definitivo: ' + Base + '€');
+      document.getElementById('Price').innerHTML = 'Il Tuo Kilometraggio: ' + Km + ' Km;' + '<br>' + 'La tua Età: ' + Age + ' Anni;' + '<br>' + 'Prezzo Base: ' + Base + ' €;' + '<br>' + 'Nessuno Sconto Avviabile: ' + Base + ' €;' + '<br>' + 'Grazie per averci scelti!' + '<br>' + 'Buonagiornata!';
     }
   }
 }
